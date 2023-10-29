d2fcc62bc4461a11a0440578a362ab22b161cac7
Jheyson-Dev <Jheyson-Dev@users.noreply.github.com>
2023-10-28 20:43:57 -0500

    Ahora si sube a cloudinary los documentos

--- src/routes/certificado/certificado.js
+++ src/routes/certificado/certificado.js
@@ -4,6 +4,9 @@ const storage = require("../../utils/multer_config");
 const upload = multer({ storage: storage });
 const certificadoController = require("../../controllers/certificado/certificado.js");
 
+const cloudinary = require("../../utils/cloudinary_config");
+require("dotenv").config();
+
 const router = new Router();
 
 router.get("/all", certificadoController.getAll);
@@ -11,8 +14,15 @@ router.get("/:id", certificadoController.getById);
 router.post(
   "/upload-doc",
   upload.single("certificado"),
-  (req, res, next) => {
-    // console.log(req.file);
+  async (req, res, next) => {
+    try {
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        public_id: "home/" + req.file.filename,
+      });
+      return res.status(200).json({ data: result });
+    } catch (err) {
+      return res.status(500).json({ error: err, message: err.message });
+    }
   },
   //   certificadoController.pushDocument,
 );
--- src/utils/cloudinary_config.js
+++ src/utils/cloudinary_config.js
@@ -4,8 +4,8 @@ config();
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLAVE_API,
-  api_secret: process.env.SECRET_API,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
 module.exports = cloudinary;
