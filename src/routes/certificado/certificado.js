a39cd4da6e70ca3947dfd3384ba23640b9a7a624
Jheyson-Dev <Jheyson-Dev@users.noreply.github.com>
2023-10-28 20:26:03 -0500

    Creacion de la funcion de almacenar con multer

--- src/routes/certificado/certificado.js
+++ src/routes/certificado/certificado.js
@@ -1,11 +1,21 @@
 const { Router } = require("express");
+const multer = require("multer");
+const storage = require("../../utils/multer_config");
+const upload = multer({ storage: storage });
 const certificadoController = require("../../controllers/certificado/certificado.js");
 
 const router = new Router();
 
 router.get("/all", certificadoController.getAll);
 router.get("/:id", certificadoController.getById);
-router.post("/upload-doc", certificadoController.pushDocument);
+router.post(
+  "/upload-doc",
+  upload.single("certificado"),
+  (req, res, next) => {
+    // console.log(req.file);
+  },
+  //   certificadoController.pushDocument,
+);
 router.post("/", certificadoController.create);
 router.put("/:id", certificadoController.update);
 router.delete("/:id", certificadoController.destroy);
---
+++ src/utils/multer_config.js
@@ -0,0 +1,14 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, fil, callback) => {
+    callback(null, "./uploads");
+  },
+  filename: (req, file, callback) => {
+    const extension = file.originalname.split(".")[1];
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    callback(null, file.fieldname + "-" + uniqueSuffix + "." + extension);
+  },
+});
+
+module.exports = storage;
